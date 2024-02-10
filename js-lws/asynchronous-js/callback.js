ffc190f9f8bf6486a83a3ce133704c1fb7da63b9
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2024-02-10 08:26:16 +0600

    sequence control problem in function method 2

--- js-lws/asynchronous-js/callback.js
+++ js-lws/asynchronous-js/callback.js
@@ -23,3 +23,12 @@ function myCalculator(num1, num2) {
 const result = myCalculator(2, 4);
 myDisplay(result);
 
+// Sequence Control : Method 2
+// The problem with the second example, is that you cannot prevent the calculator function from displaying the result.
+
+function myCalculator(num1, num2) {
+  let sum = 0;
+  sum = num1 + num2;
+  myDisplay(sum);
+}
+myCalculator(2, 5);
