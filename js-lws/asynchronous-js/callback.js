c0c4b956b05f08b7e6e7096361290e13b841a913
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2024-02-10 08:22:22 +0600

    sequence control problem in function method 1

--- js-lws/asynchronous-js/callback.js
+++ js-lws/asynchronous-js/callback.js
@@ -1,3 +1,4 @@
+// Normal Function
 function myFirst() {
   console.log("This is my first");
 }
@@ -7,3 +8,18 @@ function mySecond() {
 
 myFirst();
 mySecond();
+
+// Sequence Control : Method 1
+// The problem with this example is that you have to call two functions to display the result.
+function myDisplay(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+function myCalculator(num1, num2) {
+  let sum = 0;
+  sum = num1 + num2;
+  return sum;
+}
+const result = myCalculator(2, 4);
+myDisplay(result);
+
