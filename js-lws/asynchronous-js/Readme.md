0a94e1d8b9534004297c0bca71c971dba64efb86
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2024-02-11 21:27:10 +0600

    Can we get Can we get promise value in a variable?

---
+++ js-lws/asynchronous-js/Readme.md
@@ -0,0 +1,3 @@
+# Note Links:'
+
+- [Promise Constructor](https://www.notion.so/rakibulhashan/Promise-Constructor-4500a89cc58b424190947d02aa772bf4?pvs=4)
--- js-lws/asynchronous-js/promise.js
+++ js-lws/asynchronous-js/promise.js
@@ -1,38 +1,71 @@
-// Exmple 01
-const promise = new Promise(function (resolve, reject) {
-  // do async task
-  // DB Calls, cryptography, network
-  console.log("Promise is pending");
+// // Exmple 01
+// const promise = new Promise(function (resolve, reject) {
+//   // do async task
+//   // DB Calls, cryptography, network
+//   console.log("Promise is pending 1");
+//   setTimeout(() => {
+//     console.log("Async task 01 is complete");
+//   }, 0);
+//   something();
+//   resolve();
+// });
+
+// function something() {
+//   setTimeout(() => {
+//     console.log("inside a SOMETHING : PROMISE 01");
+//   }, 0);
+// }
+// promise.then(function () {
+//   console.log("Promise 01 Comsumed");
+// });
+
+// // Example 02
+// new Promise(function (resolve, reject) {
+//   console.log("Promise 2 Producing");
+//   resolve();
+//   setTimeout(() => {
+//     console.log("Async task 02 is complete");
+//   }, 3000);
+// }).then(function () {
+//   console.log("Promise 2 consumed");
+// });
+
+// // Example 03: Pass data through resolve function
+// const promise_03 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve({
+//       name: "Rakib",
+//       age: 27,
+//     });
+//   }, 3000);
+// });
+
+// promise_03
+//   .then(function (user) {
+//     console.log("Nothing in the first line");
+//     return user
+//   })
+//   .then((user) => {
+//     console.log("2nd Line: ", user);
+//   });
+
+// Example 04: Can we get promise value in a variable?
+// Ans: can not get the value directly. But it will show pending state.
+
+const promise_04 = new Promise(function (resolve, reject) {
   setTimeout(() => {
-    resolve();
-    console.log("Async task is complete");
-  }, 3000);
+    const error = false;
+    if (!error) {
+      resolve({ username: "rakib", pass: "123" });
+    } else {
+      reject("ERROR: Something went wrong!");
+    }
+  }, 1400);
 });
 
-promise.then(function () {
-  console.log("Promise Comsumed");
-});
-
-// Example 02
-new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    console.log("Task 2 complete");
-    resolve();
-  }, 2000);
-}).then(function () {
-  console.log("Promise 2 consumed");
-});
-
-// Example 03:
-const promise_03 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    resolve({
-      name: "Rakib",
-      age: 27,
-    });
-  }, 3000);
-});
-
-promise_03.then(function (user) {
+const userName = promise_04.then(function (user) {
   console.log(user);
+  return user.username;
 });
+
+console.log(userName);
